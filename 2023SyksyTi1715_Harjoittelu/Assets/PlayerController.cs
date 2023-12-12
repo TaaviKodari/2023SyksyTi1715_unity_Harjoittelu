833172b689e3a90f0313fced08ed4e8ccf882c2b
Your Name <you@example.com>
2023-12-12 17:21:45 +0200

    unohtunut

--- 2023SyksyTi1715_Harjoittelu/Assets/PlayerController.cs
+++ 2023SyksyTi1715_Harjoittelu/Assets/PlayerController.cs
@@ -7,10 +7,18 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 4.5f;
     public CharacterController controller;
     public float mouseSpeed = 3f;
+    public float maxLookAngle = 60f;
+    public float minLookAngle = -60f;
+    public float gravity = -10f;
+    public float jumpHeight = 3f;
     public CursorLockMode cursorLock;
     private float mouseMovementX = 0f;
     private float mouseMovementY = 0f;
     Vector3 moveDir;
+    Vector3 velocity;
+    [SerializeField]
+    bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,11 +29,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = controller.isGrounded;
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
+
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         mouseMovementX += Input.GetAxis("Mouse X") * mouseSpeed;
         mouseMovementY -= Input.GetAxis("Mouse Y") * mouseSpeed;
+
+        mouseMovementY = Mathf.Clamp(mouseMovementY,minLookAngle, maxLookAngle);
 
         Camera.main.transform.localRotation = Quaternion.Euler(mouseMovementY, 0, 0);
 
@@ -34,7 +49,14 @@ public class PlayerController : MonoBehaviour
         moveDir = new Vector3(horizontal,0,vertical);
         moveDir = transform.rotation * moveDir;
 
-        controller.SimpleMove(moveDir * moveSpeed);
+        controller.Move(moveDir * moveSpeed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
 
     }
 }
